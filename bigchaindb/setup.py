ea204b75d953e4d7a4409a8abe7cef9937826be1
vedantsahai18 <vedantsahai18@gmail.com>
2021-04-19 20:40:32 +0530

    bigchain db filed added

--- bigchaindb/setup.py
+++ bigchaindb/setup.py
@@ -9,9 +9,9 @@ BigchainDB: The Blockchain Database
 For full docs visit https://docs.bigchaindb.com
 
 """
-from setuptools import setup, find_packages
-import sys
 
+import sys
+from setuptools import setup, find_packages
 
 if sys.version_info < (3, 6):
     sys.exit('Please use Python version 3.6 or higher.')
@@ -21,17 +21,16 @@ version = {}
 with open('bigchaindb/version.py') as fp:
     exec(fp.read(), version)
 
-
-# check if setuptools is up to date
 def check_setuptools_features():
+    """Check if setuptools is up to date."""
     import pkg_resources
     try:
         list(pkg_resources.parse_requirements('foo~=1.0'))
     except ValueError:
-        exit('Your Python distribution comes with an incompatible version '
-             'of `setuptools`. Please run:\n'
-             ' $ pip3 install --upgrade setuptools\n'
-             'and then run this command again')
+        sys.exit('Your Python distribution comes with an incompatible version '
+                 'of `setuptools`. Please run:\n'
+                 ' $ pip3 install --upgrade setuptools\n'
+                 'and then run this command again')
 
 
 check_setuptools_features()
@@ -60,8 +59,9 @@ tests_require = [
     'flake8',
     'flake8-quotes==0.8.1',
     'hypothesis>=5.3.0',
+    # Removed pylint because its GPL license isn't Apache2-compatible
     'pytest>=3.0.0',
-    'pytest-cov>=2.2.1',
+    'pytest-cov==2.8.1',
     'pytest-mock',
     'pytest-xdist',
     'pytest-flask',
@@ -71,21 +71,22 @@ tests_require = [
 ] + docs_require
 
 install_requires = [
-    'pymongo~=3.6',
+    'aiohttp==3.6.2',
+    'bigchaindb-abci==1.0.5',
     'cryptoconditions==0.8.0',
-    'python-rapidjson~=0.6.0',
-    'logstats~=0.2.1',
-    'flask==1.0.0',
-    'flask-cors~=3.0.0',
-    'flask-restful~=0.3.0',
-    'requests~=2.20.0',
-    'gunicorn~=19.0',
-    'jsonschema~=2.5.1',
-    'pyyaml>=4.2b1',
-    'aiohttp~=3.0',
-    'bigchaindb-abci>=1.0.2',
-    'setproctitle~=1.1.0',
-    'packaging~=18.0',
+    'flask-cors==3.0.8',
+    'flask-restful==0.3.8',
+    'flask==1.1.2',
+    'gunicorn==20.0.4',
+    'jsonschema==3.2.0',
+    'logstats==0.3.0',
+    'packaging>=20.0.0',  # Dockerfile-alpine required for pkg has ver 20.0.4
+    # TODO Consider not installing the db drivers, or putting them in extras.
+    'pymongo==3.7.2',
+    'python-rapidjson==0.9.1',
+    'pyyaml==5.3.1',
+    'requests==2.23.0',
+    'setproctitle==1.1.10',
 ]
 
 if sys.version_info < (3, 6):
@@ -107,7 +108,7 @@ setup(
         ),
     url='https://github.com/BigchainDB/bigchaindb/',
     author='BigchainDB Contributors',
-    author_email='devs@bigchaindb.com',
+    author_email='contact@ipdb.global',
     license='Apache Software License 2.0',
     zip_safe=False,
     python_requires='>=3.6',
@@ -120,15 +121,16 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
     ],
 
     packages=find_packages(exclude=['tests*']),
 
-    scripts = ['pkg/scripts/bigchaindb-monit-config'],
+    scripts=['pkg/scripts/bigchaindb-monit-config'],
 
     entry_points={
         'console_scripts': [
