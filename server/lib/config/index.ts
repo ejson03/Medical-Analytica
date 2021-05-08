00d58d16ac9f8847d2b136a90700bcde43fc364c
Pratik Chowdhury <pratikc@live.co.uk>
2021-05-08 13:30:15 +0000

    [bugfix] Optional value should be afterwards

--- server/lib/config/index.ts
+++ server/lib/config/index.ts
@@ -3,11 +3,11 @@
 // dotenv.config();
 
 export const PORT: number = Number(process.env.PORT || 5000);
-export const SECRET: string = String('secret_default' || process.env.SECRET);
+export const SECRET: string = String(process.env.SECRET || 'secret_default');
 export const JWT_SECRET: string = String(process.env.JWT_SECRET);
 export const VAULT: any = {
-   url: String('http://localhost:8200' || process.env.VAULT_URL),
-   token: String('myroot' || process.env.VAULT_TOKEN)
+   url: String(process.env.VAULT_URL || 'http://localhost:8200' ),
+   token: String(process.env.VAULT_TOKEN || 'myroot')
 };
 export const MONGO_URL: string = String(process.env.MONGO_URL || 'mongodb://localhost:27017/');
 export const RASA_URL: string = String(process.env.RASA_URL || 'http://localhost:5005');
