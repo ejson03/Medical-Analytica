d4d2c55e50c3923927cd12050d816dbfbc8e8a92
Pratik Chowdhury <pratikc@live.co.uk>
2021-05-08 11:09:43 +0000

    [Config] Set localhost as default

--- server/lib/config/index.ts
+++ server/lib/config/index.ts
@@ -3,15 +3,15 @@
 // dotenv.config();
 
 export const PORT: number = Number(process.env.PORT || 5000);
-export const SECRET: string = String(process.env.SECRET);
+export const SECRET: string = String('secret_default' || process.env.SECRET);
 export const JWT_SECRET: string = String(process.env.JWT_SECRET);
 export const VAULT: any = {
-   url: String(process.env.VAULT_URL),
-   token: String(process.env.VAULT_TOKEN)
+   url: String('http://localhost:8200' || process.env.VAULT_URL),
+   token: String('myroot' || process.env.VAULT_TOKEN)
 };
-export const MONGO_URL: string = String(process.env.MONGO_URL || 'mongodb://mongo:27017/');
-export const RASA_URL: string = String(process.env.RASA_URL || 'http://server:5005');
-export const BIGCHAIN_URL: string = String(process.env.BIGCHAIN_URL || 'http://bigchaindb:9984/api/v1');
+export const MONGO_URL: string = String(process.env.MONGO_URL || 'mongodb://localhost:27017/');
+export const RASA_URL: string = String(process.env.RASA_URL || 'http://localhost:5005');
+export const BIGCHAIN_URL: string = String(process.env.BIGCHAIN_URL || 'http://localhost:9984/api/v1/');
 export const IPFS: any = {
    url: String(process.env.IPFS_URL || 'ipfs.infura.io'),
    port: String('5001')
