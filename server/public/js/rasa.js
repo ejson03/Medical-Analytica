6c8a6e6b6c325877bcb73b72ec1c45fbe51ea174
vedantsahai18 <vedantsahai18@gmail.com>
2021-05-15 16:55:37 +0530

    rasa js and assets ejs css changes

--- server/public/js/rasa.js
+++ server/public/js/rasa.js
@@ -352,7 +352,7 @@ function setBotResponse(val) {
                            <td>
                              <form method="post" action="/view">
                               <input type="hidden" name="status" value="encrypted"> 
-                                <button type="submit" rel="tooltip" title="File" class="btn btn-danger btn-link btn-sm" value="${record.data.file}" name="fileURL" target="_blank">
+                                <button type="submit" rel="tooltip" title="File" class="btn btn-danger btn-link btn-sm" value="${record.data.file}" name="fileURL" formtarget="_blank">
                                  <i class="material-icons">attachment</i>
                                 </button>
                              </form> 
